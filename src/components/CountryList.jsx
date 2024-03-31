212b7c09503b3e61342c8e13d5830c5a87e1e910
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-03-31 13:18:12 +0200

    Refactor code with context API and Add Back button component to other UI pieces

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -19,8 +22,8 @@ function CountryList({ cities, isLoading }) {
 
   return (
     <ul className={styles.countryList}>
-      {countries.map((country) => (
-        <CountryItem country={country} key={country.id} />
+      {countries.map((country, index) => (
+        <CountryItem country={country} key={`${country.country}-${index}`} />
       ))}
     </ul>
   );
